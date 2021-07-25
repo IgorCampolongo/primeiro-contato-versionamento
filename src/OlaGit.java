0b9e74865f2d3522937541b445b39f09d52bd77b
IRRGO <igorcampolongo@gmail.com>
2021-07-25 13:49:54 -0300

    teste de segundo commit

--- src/OlaGit.java
+++ src/OlaGit.java
@@ -2,7 +2,21 @@
 public class OlaGit {
 
 	public static void main(String[] args) {
-		System.out.println("Teste de primeiro versionamento de projeto");
+		mensagem msg = new mensagem();
+		System.out.println(msg.getMensagem());
 	}
 
+}
+
+
+	
+class mensagem{
+	
+	String mensagem = "Teste de primeiro versionamento de projeto";
+	
+	public String getMensagem() {
+		return mensagem;
+	}
+	
+	
 }
